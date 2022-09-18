8eb9079df3d50f6d06ef44831f87c8b240cbd2c9
youssef ghamry <77971093+youssefghamry@users.noreply.github.com>
2022-09-18 21:41:51 +0200

    Add files via upload

---
+++ js/images.js
@@ -0,0 +1,50 @@
+let images = document.getElementById('div55');
+
+document.querySelector('.button1').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/1.svg'; 
+    }
+    document.querySelector('.button2').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/2.svg';
+    }
+    document.querySelector('.button3').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/3.svg';
+    }
+    document.querySelector('.button4').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/4.svg';
+    }
+    document.querySelector('.button5').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/5.svg';
+    }
+    document.querySelector('.button6').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/6.svg';
+    }
+    document.querySelector('.button7').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/7.svg';
+    }
+    document.querySelector('.button8').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/8.svg';
+    }
+    document.querySelector('.button9').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/9.svg';
+    }
+    document.querySelector('.button10').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/10.svg';
+    }
+    document.querySelector('.button11').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/11.svg';
+    }
+    document.querySelector('.button12').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/12.svg';
+    }
+    document.querySelector('.button13').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/13.svg';
+    }
+    document.querySelector('.button14').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/14.svg';
+    }  
+    document.querySelector('.button14').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/15.svg';
+    }
+      document.querySelector('.button14').onclick = function() {
+        images.innerHTML = document.images[0].src = 'LOGO/16.svg';
+    }
